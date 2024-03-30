7a6b5986925802242588ef9839261954e3a344f8
PeppinoFreeman <compte.surprise02@gmail.com>
2024-03-30 14:12:26 +0100

    updated prod environment variables

--- Module_Angular/src/environments/environment.prod.ts
+++ Module_Angular/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   name: 'production',
-  apiUrl: 'https://webapplication120240318123549.azurewebsites.net/',
+  apiUrl: 'https://webapplication12014.azurewebsites.net/',
   caseEndpoint: 'api/Case',
   segmentEndpoint: 'api/Segment',
   defaultLanguage: 'en',
